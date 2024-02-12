2bc417292a3d356365e7175871ac470027a36dd7
CrowDev <h.aguilarvalenzuela@gmail.com>
2024-02-12 06:36:52 -0300

    display current saved cities

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { CityTable } from 'src/app/@types/types';
+import { TemporalDbService } from 'src/app/services/temporal-db.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,4 +11,35 @@ import { Component } from '@angular/core';
 })
 export class ProfileComponent {
 
+  dataSource: CityTable[] = [];
+  pageSize = 10;
+
+  currentPage = 1;
+  totalPages = 1;
+  displayedData: CityTable[] = [];
+  constructor(private db: TemporalDbService) { }
+
+  ngOnInit() {
+    this.getCities();
+  }
+
+  getCities() {
+    this.dataSource = this.db.getCities();
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.displayedData = this.dataSource.slice(startIndex, endIndex);
+    this.totalPages = Math.ceil(this.dataSource.length / this.pageSize);
+  }
+
+  handleRemoveCity(city: CityTable) {
+    this.db.removeCity(city.id);
+    this.getCities();
+  }
+
+  onPaginate(page: PageEvent) {
+    const { pageSize, pageIndex } = page;
+    this.pageSize = pageSize;
+    this.currentPage = pageIndex + 1;
+    this.getCities();
+  }
 }
